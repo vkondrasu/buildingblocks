2e18a481f58ce13d9c2ab844009d56f28d192a4d
venkateswar.kondrasu <venkateswar.kondrasu@servicenow.com>
2022-03-31 21:45:37 +0530

    Updated snakes and ladders objects configurable

--- SnakeAndLadder/board.py
+++ SnakeAndLadder/board.py
@@ -1,35 +1,15 @@
 import random
 
 class Board:
-    def __init__(self, endPosition):
+    def __init__(self, endPosition, laddersMap, snakesMap):
         self.endPosition = endPosition
         self.players ={}
         self.endOfGame = False
         self.winner = None
         self.dice = Dice()
         self.playerId = 0
-
-        #make ladders and snakes configurable values
-
-        self.ladders = {}
-        self.ladders[2] = 23
-        self.ladders[8] = 12
-        self.ladders[17] = 93
-        self.ladders[29] = 54
-        self.ladders[32] = 51
-        self.ladders[39] = 80
-        self.ladders[62] = 78
-        self.ladders[70] = 89
-        self.ladders[75] = 96
-
-        self.snakes = {}
-        self.snakes[99] = 5
-        self.snakes[92] = 76
-        self.snakes[83] = 80
-        self.snakes[67] = 50
-        self.snakes[59] = 37
-        self.snakes[41] = 20
-        self.snakes[31] = 14
+        self.ladders = laddersMap
+        self.snakes = snakesMap
 
     def rollDice(self, player_id):
         move = self.dice.rollDice()
@@ -75,8 +55,31 @@ class Dice:
     def rollDice(self):
         return random.randrange(1, 7)
 
+ladders_map = {2:23, 8:12, 17:93, 29:54, 32:51, 39:80, 62:78, 70:89, 75:96}
+'''
+        self.ladders[2] = 23
+        self.ladders[8] = 12
+        self.ladders[17] = 93
+        self.ladders[29] = 54
+        self.ladders[32] = 51
+        self.ladders[39] = 80
+        self.ladders[62] = 78
+        self.ladders[70] = 89
+        self.ladders[75] = 96
+        '''
 
-b = Board(100)
+snakes_map = {99:5, 92:76, 83:80, 67:50, 59:37, 41:20, 31:14}
+'''
+        self.snakes[99] = 5
+        self.snakes[92] = 76
+        self.snakes[83] = 80
+        self.snakes[67] = 50
+        self.snakes[59] = 37
+        self.snakes[41] = 20
+        self.snakes[31] = 14
+        '''
+b = Board(100, ladders_map, snakes_map)
+b.addPlayer()
 b.addPlayer()
 b.addPlayer()
 b.addPlayer()
